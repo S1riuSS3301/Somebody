8512bbbfb40a0229cb07ccf64bc871f46a9b63b9
{Koks} <koks@blue-creature.com>
2023-10-29 22:56:27 +0600

    Remove Ratvar function

--- Content.Server/Ratvar/RatvarComponent.cs
+++ Content.Server/Ratvar/RatvarComponent.cs
@@ -1,4 +1,3 @@
-using Content.Shared.Actions.ActionTypes;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 
@@ -6,11 +5,5 @@ namespace Content.Server.Ratvar
 {
     [RegisterComponent]
     public sealed partial class RatvarComponent : Component
-    {
-        /// <summary>
-        ///     The action for the Punish with Lightning
-        /// </summary>
-        [DataField("actionPunishLightning", required: false)]
-        public InstantAction ActionPunishLightning = new();
-    }
+    { }
 }
